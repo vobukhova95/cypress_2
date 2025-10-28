4bc86e75749d56c767c1ef50ed8d01ac38093ab0
Veronika Obukhova <vera.obukhova.95@gmail.com>
2025-10-29 00:52:10 +0700

    CI/CD change pg

--- cypress/e2e/admin/adminAuth.cy.js
+++ cypress/e2e/admin/adminAuth.cy.js
@@ -14,18 +14,4 @@ describe('Testing the login admin', () => {
 		cy.login(user.validEmail, user.validPassword);
 		cy.contains(mainText).should('be.visible');
 	});
-	it('Should not log in with incorrect login and password', () => {
-		const errorText = 'Ошибка авторизации!';
-
-		cy.login(user.invalidEmail, user.invalidPassword);
-		cy.contains(errorText).should('be.visible');
-	});
-
-	it('Should warn if email is empty', () => {
-		cy.login(' ', user.validPassword);
-		cy.get(selector.email)
-			.then(el => {
-				return el[0].checkValidity();
-			}).should('be.false');
-	});
 });
---
+++ cypress/e2e/admin/adminAuthError.cy.js
@@ -0,0 +1,25 @@
+const user = require('../fixtures/users.json');
+const selector = require('../fixtures/loginPageSelectors.json')
+
+
+describe('Testing the login admin', () => {
+
+	beforeEach(() => {
+		cy.visit('https://qamid.tmweb.ru/admin/');
+	});
+
+	it('Should not log in with incorrect login and password', () => {
+		const errorText = 'Ошибка авторизации!';
+
+		cy.login(user.invalidEmail, user.invalidPassword);
+		cy.contains(errorText).should('be.visible');
+	});
+
+	it('Should warn if email is empty', () => {
+		cy.login(' ', user.validPassword);
+		cy.get(selector.email)
+			.then(el => {
+				return el[0].checkValidity();
+			}).should('be.false');
+	});
+});
